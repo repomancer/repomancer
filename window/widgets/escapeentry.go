1d7750b20e51936db8c79c401ad8e0ea2705f829
Jason Short <jason@sheersky.com>
2025-02-15 19:41:24 -0800

    Add repository dialog working

---
+++ window/widgets/escapeentry.go
@@ -0,0 +1,34 @@
+package widgets
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
+
+type EscapeEntry struct {
+	widget.Entry
+	OnEscape  func()
+	MaxLength int
+}
+
+func (m *EscapeEntry) TypedKey(event *fyne.KeyEvent) {
+	if event.Name == fyne.KeyEscape && m.OnEscape != nil {
+		m.OnEscape()
+	}
+	if event.Name == fyne.KeyEnter || event.Name == fyne.KeyReturn && m.OnSubmitted != nil {
+		m.OnSubmitted(m.Text)
+	}
+}
+
+func (m *EscapeEntry) TypedRune(r rune) {
+	if m.MaxLength == -1 || len(m.Text) < m.MaxLength {
+		m.Entry.TypedRune(r)
+	}
+}
+
+func NewEscapeEntry() *EscapeEntry {
+	item := &EscapeEntry{}
+	item.ExtendBaseWidget(item)
+	item.MaxLength = -1
+	return item
+}
