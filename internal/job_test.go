e412ac11b62ce4f0b99b844609043afc95b27fc4
Jason Short <jason@sheersky.com>
2025-02-17 18:31:04 -0800

    Add Job tests

---
+++ internal/job_test.go
@@ -0,0 +1,42 @@
+package internal
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func NewTestRepository() *Repository {
+	r := Repository{}
+	return &r
+}
+
+func TestNewJob(t *testing.T) {
+	r := NewTestRepository()
+	j := NewJob(r, "ls")
+	assert.Equal(t, j.Finished, false)
+	assert.Equal(t, j.InternalCommand, false)
+	assert.Equal(t, j.Command, "ls")
+
+	j.Run()
+	assert.Equal(t, j.Finished, true)
+	assert.NotNil(t, j.StartTime)
+	assert.NotNil(t, j.EndTime)
+}
+
+func TestJobCommandFailed(t *testing.T) {
+	r := NewTestRepository()
+	j := NewJob(r, "ThisDoesNotExist")
+	j.Run()
+	assert.Equal(t, j.Finished, true)
+	assert.Equal(t, j.StdOut, []string{"bash: ThisDoesNotExist: command not found"})
+	assert.NotNil(t, j.Error)
+	assert.True(t, strings.HasSuffix(j.Duration(), "ms"))
+}
+
+func TestNewInternalJob(t *testing.T) {
+	r := NewTestRepository()
+	j := NewInternalJob(r, "ls")
+	assert.Equal(t, j.Finished, false)
+	assert.Equal(t, j.InternalCommand, true)
+}
